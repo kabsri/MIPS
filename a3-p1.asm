d8905706a203fff7c607c26197b0042a888ca5cc
Kabilan Sriranjan <kabilan.s97@gmail.com>
2017-11-17 00:18:51 -0500

    I think i got a function to build a linked list

--- a3-p1.asm
+++ a3-p1.asm
@@ -1,7 +1,9 @@
-#StudentID:
-#Name:
+#StudentID: 260671847
+#Name: Kabilan Sriranjan
 
 .data
+initialPrompt:	.asciiz "Enter characters to add to list. Use * to terminate list\n"
+charPrompt:	.asciiz "\nEnter the next character\n"	 
 
 .text
 #There are no real limit as to what you can use
@@ -12,6 +14,10 @@
 #deducted marks.
 
 main:
+	jal build
+	
+	li $v0, 10
+	syscall
 #build a linked list
 #print "Original linked list\n"
 #print the original linked list
@@ -25,6 +31,50 @@ build:
 #continually ask for user input UNTIL user inputs "*"
 #FOR EACH user inputted character inG, create a new node that hold's inG AND an address for the next node
 #at the end of build, return the address of the first node to $v1
+
+	li $s0, '*'	#save '*' character to $s0 as it is important
+	
+	la $a0, initialPrompt 	#load prompt to $a0
+	li $v0, 4
+	syscall 	#print prompt on how to use method
+	
+	li $v0, 12
+	syscall 	#read character
+	move $t0, $v0	#save input character to $t0
+	
+	beq $s0, $v0, tail 	#if user entered a * then make tail
+	
+	li $a0, 8	#store 8 into $a0 to allocate 8 bytes to a node
+	li $v0, 9
+	syscall		#allocate 8 bytes to memory
+	move $t2, $v0	#store head of list in $t2 so we can return it later
+	
+	move $t1, $v0 		#store address of malloc to $t1
+	sw $t0, 4($t1)		#save character to second half of node
+	
+loop:
+	la $a0, charPrompt	#load prompt to $a0
+	li $v0, 4
+	syscall			#print prompt for getting next character
+	
+	li $v0, 12
+	syscall 	#read character
+	move $t0, $v0	#save input character to $t0
+	
+	beq $s0, $v0, tail	#if user entered a * then make tail
+	
+	li $a0, 8	#store 8 into $a0 to allocate 8 bytes to a node
+	li $v0, 9
+	syscall		#allocate 8 bytes of memory
+	
+	la $v0, 0($t1)		#save location of current node in previous node
+	move $t1, $v0		#store pointer to current node to $t1
+	sw $t0, 4($t1)		#save character to second half of node
+	j loop
+
+tail:	
+	move $v0, $t2
+	jr $ra
 
 print:
 #$a0 takes the address of the first node
