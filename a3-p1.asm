a6b31593bf15de0188ff5b9d7561f974fa4b9c2d
Kabilan Sriranjan <kabilan.s97@gmail.com>
2017-11-22 19:42:41 -0500

    Finished everything

--- a3-p1.asm
+++ a3-p1.asm
@@ -5,6 +5,8 @@
 initialPrompt:	.asciiz "Enter characters to add to list. Use * to terminate list\n"
 charPrompt:	.asciiz "\nEnter the next character\n"
 newLine:	.asciiz "\n" 
+original:	.asciiz "\nOriginal linked list\n"
+reversed:	.asciiz "\nReversed linked list\n"
 
 .text
 #There are no real limit as to what you can use
@@ -15,45 +17,57 @@ newLine:	.asciiz "\n"
 #deducted marks.
 
 main:
-	jal build
-	move $v0, $a0
-	jal print
-	
-	li $v0, 10
-	syscall
 #build a linked list
 #print "Original linked list\n"
 #print the original linked list
+	jal build		#build linked list from user input
+	move $s0, $v1		#save linked list head to $s0
+	la $a0, original
+	li $v0, 4
+	syscall			#print "original linked list"
+	move $a0, $s0		#put head of list in $a0
+	jal print		#print original list
 
 #reverse the linked list
 #On a new line, print "reversed linked list\n"
 #print the reversed linked list
-#terminate program
+#terminate program	
+	move $a1, $s0		#put linked list in $a1 to reverse
+	jal reverse		#reverse the linked list
+	move $s1, $v1		#save reversed list to $s0
+	la $a0, reversed
+	li $v0, 4
+	syscall			#print "reversed linked list"
+	move $a0, $s1		#put head of list in $a0
+	jal print		#print reversed list
+	
+	li $v0, 10
+	syscall			#terminate program
+
+
 
 build:
 #continually ask for user input UNTIL user inputs "*"
 #FOR EACH user inputted character inG, create a new node that hold's inG AND an address for the next node
 #at the end of build, return the address of the first node to $v1
 
-	li $s0, '*'	#save '*' character to $s0 as it is important
+	li $s0, '*'		#save '*' character to $s0 as it is important
 	
 	la $a0, initialPrompt 	#load prompt to $a0
 	li $v0, 4
-	syscall 	#print prompt on how to use method
+	syscall 		#print prompt on how to use method
 	
 	li $v0, 12
-	syscall 	#read character
-	move $t0, $v0	#save input character to $t0
+	syscall 		#read character
+	move $t0, $v0		#save input character to $t0
 	
-	beq $s0, $v0, tail 	#if user entered a * then make tail
+	beq $s0, $t0, empty 	#if user entered a * at the beginning create an empty list
 	
-	li $a0, 8	#store 8 into $a0 to allocate 8 bytes to a node
+	li $a0, 8		#store 8 into $a0 to allocate 8 bytes to a node
 	li $v0, 9
-	syscall		#allocate 8 bytes to memory
-	lw $s0, 0($v0)
-	lw $s1, 4($v0)
-	move $t2, $v0	#store head of list in $t2 so we can return it later
+	syscall			#allocate 8 bytes to memory
 	
+	move $t2, $v0		#store head of list in $t2 so we can return it later
 	move $t1, $v0 		#store address of malloc to $t1
 	sw $t0, 4($t1)		#save character to second half of node
 	
@@ -63,43 +77,73 @@ loop:
 	syscall			#print prompt for getting next character
 	
 	li $v0, 12
-	syscall 	#read character
-	move $t0, $v0	#save input character to $t0
+	syscall 		#read character
+	move $t0, $v0		#save input character to $t0
 	
-	beq $s0, $v0, tail	#if user entered a * then make tail
+	beq $s0, $t0, tail	#if user entered a * then make tail
 	
-	li $a0, 8	#store 8 into $a0 to allocate 8 bytes to a node
+	li $a0, 8		#store 8 into $a0 to allocate 8 bytes to a node
 	li $v0, 9
-	syscall		#allocate 8 bytes of memory
+	syscall			#allocate 8 bytes of memory
 	
 	sw $v0, 0($t1)		#save location of current node in previous node
 	move $t1, $v0		#store pointer to current node to $t1
 	sw $t0, 4($t1)		#save character to second half of node
 	j loop
-
 tail:	
-	move $v0, $t2		#save address of head to $v0 to return it
+	move $v1, $t2		#save address of head to $v0 to return it
+	jr $ra
+empty:
+	move $v1, $0
 	jr $ra
 
-print:
 #$a0 takes the address of the first node
 #prints the contents of each node in order
-	move $t0, $a0		#save address of current node to $t0
-	beq $t0, $0, end	#check if address is a null pointer
-	lb $a0, 4($t0)		#load the character we need to print to $a0
+print:
+	move $t0, $a0		#save first node to $t0
+	la $a0, newLine
+	li $v0, 4
+	syscall			#print new line before anything is printed
+	move $a0, $t0		#put original $a0 back
+	beq $a0, $0 endPrint
+next:
+	move $t0, $a0		#save current node to $t0
+	lw $a0, 4($t0)		#load the character we need to print to $a0
 	li $v0, 11
 	syscall			#print the character
 	la $a0, newLine		#load new line character to $a0
 	li $v0, 4
 	syscall			#print new line
 	lw $a0, 0($t0)		#store address of next node to $a0
-	j print
-	
-
-end:
-	jr $ra
+	beq $a0, $0, endPrint	#if there is no next node then just end procedure
+	j next
+endPrint:
+	jr $ra			#return
 
 reverse:
+	beq $a1, $0, revEmpty	#if list is empty then return empty list
+	lw $t0, 0($a1)		#save head to $t0
+	beq $t0, $0, revOne	#if list has only one element we don't need to change anything
+	sw $0, 0($a1)		#store next address in original head to null, as now it is the tail
+	move $t2, $a1		#save head to $t2
+revLoop:
+	lw $t1, 0($t0)		#save the next node to $t0
+	beq $t1, $0, endRev 	#if the next node is null then we are done
+	sw $t2, 0($t0)		#store the previous cell address into current one
+	move $t2, $t0		#current node is now the previous node
+	move $t0, $t1		#next node is now current node
+	j revLoop		#reverse next part of list
+	
+endRev:
+	sw $t2, 0($t0)		#save address of final node
+	move $v1, $t0
+	jr $ra			#return
+revEmpty:
+	move $v1, $0		#return empty list
+	jr $ra
+revOne:
+	move $v1, $a1		#make no changes
+	jr $ra
 #$a1 takes the address of the first node of a linked list
 #reverses all the pointers in the linked list
 #$v1 returns the address
--- a3-p3.asm
+++ a3-p3.asm
@@ -7,18 +7,27 @@
 .data
 N: .word 100
 a: .float 0
-b: .float 1
+b: .float 0.5
 error: .asciiz "error: must have low < hi\n"
 
 .text 
 ###########################################################
 main:
 	# set argument registers appropriately
-	
+	l.s $f12, a		#a will be stored into $f0
+	l.s $f13, b		#b will be stored into $f2
+	la $a0, ident
+	jal integrate
 	# call integrate on test function 
 	
 	# print result and exit with status 0
+	li $v0, 2
+	mov.s $f12, $f0
+	syscall			#print return value of integrate to screen
 	
+	li $v0, 17
+	li $a0, 0
+	syscall			#exit with status 0
 
 ###########################################################
 # float integrate(float (*func)(float x), float low, float hi)
@@ -26,12 +35,38 @@ main:
 # $f12 gets low, $f13 gets hi, $a0 gets address (label) of func
 # $f0 gets return value
 integrate: 
-	jal check
+	addi $sp, $sp, -4	#make space for return adress on stack
+	sw $ra, 0($sp)		#store return adress to stack
+	jal check		#check if a and b are valid values
 	
 	# initialize $f4 to hold N
-	# since N is declared as a word, will need to convert 
+	lw $t0, N
+	mtc1 $t0, $f4
+	# since N is declared as a word, will need to convert
+	cvt.s.w $f4, $f4
 	
-	jr $ra
+	sub.s $f6, $f13, $f12	#save b-a into $f6
+	div.s $f6, $f6, $f4	#save (b-a)/n into $f6, this is the length of each subinterval
+	
+	li $t0, 2
+	mtc1 $t0, $f8
+	cvt.s.w $f8, $f8	#save 2.0 into $f8
+	
+	div.s $f8, $f6, $f8	#save (b-a)/2n into $f8
+	add.s $f12, $f12, $f8	#save a+(b-1)/2n into $f12, the first point where we evaluate f(x)
+
+loop:
+	jalr $a0		#evaluate the function at $f12
+	mul.s $f0, $f0, $f6	#multiply result by interval length
+	add.s $f10, $f10, $f0	#add result to $f10, the cumulated result
+	add.s $f12, $f12, $f6	#new x value to evaluate f(x)
+	c.lt.s $f12, $f13	#if new x value is greater than b we are done
+	bc1t loop
+	
+	mov.s $f0, $f10
+	lw $ra 0($sp)		#load old return address back to stack
+	addi $sp, $sp, 4	#restore stack
+	jr $ra			#return
 
 ###########################################################
 # void check(float low, float hi)
@@ -39,7 +74,17 @@ integrate:
 # $f12 gets low, $f13 gets hi
 # # prints error message and exits with status 1 on fail
 check:
-	jr $ra
+	c.lt.s $f12, $f13		#check if a<b
+	bc1f failure		#go to failure if a not less than b
+	jr $ra			#return if a<b
+	
+failure:
+	la $a0, error		#load error message to $a0
+	li $v0, 4
+	syscall			#print error message
+	li $v0, 17
+	li $a0, 1
+	syscall			#exit with status 1
 
 ###########################################################
 # float ident(float x) { return x; }
